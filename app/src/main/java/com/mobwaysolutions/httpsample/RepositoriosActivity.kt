66275cbd38f0d555efb89c4f24a9f2746c5ad526
Arthur Cordova Stapassoli <arthur.stapassoli@gmail.com>
2022-06-21 15:33:51 -0300

    Pesquisa dinamica por nome

--- app/src/main/java/com/mobwaysolutions/httpsample/RepositoriosActivity.kt
+++ app/src/main/java/com/mobwaysolutions/httpsample/RepositoriosActivity.kt
@@ -2,8 +2,16 @@ package com.mobwaysolutions.httpsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.widget.AppCompatImageButton
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.progressindicator.CircularProgressIndicator
+import com.google.android.material.textfield.TextInputLayout
 import com.mobwaysolutions.httpsample.adapter.RepositorioAdapter
 import retrofit2.Call
 import retrofit2.Callback
@@ -12,18 +20,37 @@ import retrofit2.Response
 class RepositoriosActivity : AppCompatActivity(), Callback<List<RepositorioModel>> {
 
     lateinit var rvRepositorios: RecyclerView
+    lateinit var etSearch: TextInputLayout
+    lateinit var buttonSearch: AppCompatImageButton
+    lateinit var cipLoading: CircularProgressIndicator
     private val repositorioAdapter = RepositorioAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_repositorios)
 
+        etSearch = findViewById(R.id.tilSearch)
+        buttonSearch = findViewById(R.id.bSearch)
+        cipLoading = findViewById(R.id.cpiLoading)
         rvRepositorios = findViewById(R.id.rvRepositorios)
         rvRepositorios.adapter = repositorioAdapter
         rvRepositorios.layoutManager = LinearLayoutManager(this)
 
+        buttonSearch.setOnClickListener {
+            etSearch.editText?.text.let {
+                val query = it?.toString().orEmpty()
+                if (query.isNotEmpty()) {
+                    cipLoading.mostrar()
+                    repositorioAdapter.refresh(emptyList())
+                    fetchData(query)
+                }
+            }
+        }
+    }
+
+    fun fetchData(userName: String) {
         val services = RetrofitHelper.initGithubServices()
-        val call = services.repositoriosDoUsuario("arthurcordova")
+        val call = services.repositoriosDoUsuario(userName)
         call.enqueue(this)
     }
 
@@ -31,6 +58,8 @@ class RepositoriosActivity : AppCompatActivity(), Callback<List<RepositorioModel
         call: Call<List<RepositorioModel>>,
         response: Response<List<RepositorioModel>>
     ) {
+        cipLoading.esconder()
+
         val listaDerepositorios = response.body()
         listaDerepositorios?.let {
             repositorioAdapter.refresh(it)
@@ -38,5 +67,8 @@ class RepositoriosActivity : AppCompatActivity(), Callback<List<RepositorioModel
     }
 
     override fun onFailure(call: Call<List<RepositorioModel>>, t: Throwable) {
+        cipLoading.esconder()
+
+
     }
 }
---
+++ app/src/main/java/com/mobwaysolutions/httpsample/ViewExtensions.kt
@@ -0,0 +1,11 @@
+package com.mobwaysolutions.httpsample
+
+import android.view.View
+
+fun View.mostrar() {
+    visibility = View.VISIBLE
+}
+
+fun View.esconder() {
+    visibility = View.GONE
+}
